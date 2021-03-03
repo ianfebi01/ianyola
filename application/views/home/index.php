ad43d2e28ffd06f17948629b15ee58855e55e62a
Ian Febi Sastrataruna <63412349+ianfebi01@users.noreply.github.com>
2021-03-03 22:03:00 +0700

    hehe

---
+++ application/views/home/index.php
@@ -0,0 +1,92 @@
+    <!-- jumbotron -->
+    <section class="jumbotron text-center">
+      <img src="<?php echo base_url(); ?>assets/images/icon_ianyola.png" width="400" alt="ianyola" />
+      <h1 class="display-4">ianyola</h1>
+      <p class="lead">Musician | Content Creator</p>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+        <path
+          fill="#ffff"
+          fill-opacity="1"
+          d="M0,288L48,245.3C96,203,192,117,288,117.3C384,117,480,203,576,197.3C672,192,768,96,864,80C960,64,1056,128,1152,149.3C1248,171,1344,149,1392,138.7L1440,128L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"
+        ></path>
+      </svg>
+    </section>
+    <!-- jumbotron -->
+    <!-- About -->
+    <section id="about">
+      <div class="container">
+        <div class="row text-center mb-3">
+          <div class="col">
+            <h2>About Me</h2>
+          </div>
+        </div>
+        <div class="row justify-content-center fs-5 text-center">
+          <div class="col-md-4">
+            <p>Hallo! kami adalah pasangan konten kreator yang membuat konter berupa cover akustik lagu yang sedang populer dan lagu jawa.</p>
+          </div>
+          <div class="col-md-4">
+            <p>Banyak yang mengira bahwa "Ianyola" adalah nama satu orang, namun yang benar adalah "Ian" adalah yang cowok, "Yola" yang cewek.</p>
+          </div>
+        </div>
+      </div>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+        <path
+          fill="#f1f1f1"
+          fill-opacity="1"
+          d="M0,128L40,138.7C80,149,160,171,240,176C320,181,400,171,480,149.3C560,128,640,96,720,106.7C800,117,880,171,960,181.3C1040,192,1120,160,1200,128C1280,96,1360,64,1400,48L1440,32L1440,320L1400,320C1360,320,1280,320,1200,320C1120,320,1040,320,960,320C880,320,800,320,720,320C640,320,560,320,480,320C400,320,320,320,240,320C160,320,80,320,40,320L0,320Z"
+        ></path>
+      </svg>
+    </section>
+
+    <!-- About -->
+    <!-- Channel -->
+    <section id="channel">
+      <div class="container">
+        <div class="row text-center">
+          <div class="col">
+            <h2>My Channel</h2>
+          </div>
+        </div>
+        <div class="row text-center">
+          <div class="col">
+            <script src="https://apps.elfsight.com/p/platform.js" defer></script>
+            <div class="elfsight-app-a53d0e1a-c2c1-4985-a248-69ccf3b16cce"></div>
+          </div>
+        </div>
+      </div>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+        <path
+          fill="#fff"
+          fill-opacity="1"
+          d="M0,96L40,117.3C80,139,160,181,240,197.3C320,213,400,203,480,176C560,149,640,107,720,117.3C800,128,880,192,960,229.3C1040,267,1120,277,1200,250.7C1280,224,1360,160,1400,128L1440,96L1440,320L1400,320C1360,320,1280,320,1200,320C1120,320,1040,320,960,320C880,320,800,320,720,320C640,320,560,320,480,320C400,320,320,320,240,320C160,320,80,320,40,320L0,320Z"
+        ></path>
+      </svg>
+    </section>
+    <!-- Channel -->
+    <!-- Social Media -->
+    <section id="social">
+      <div class="container">
+        <div class="row text-center">
+          <div class="col">
+            <h2>Our Contact</h2>
+          </div>
+        </div>
+        <div class="social">
+          <div class="row text-center justify-content-center">
+            <div class="col">
+              <a href="https://youtube.com/c/ianyola_official"><img src="<?php echo base_url(); ?>assets/images/yt.png" alt="" width="50px" style="margin: 10px" /></a>
+              <a href="https://www.instagram.com/ianyola_official/"><img src="<?php echo base_url(); ?>assets/images/ig.png" alt="" width="50px" style="margin: 10px" /></a>
+              <a href="https://wa.me/085868563764"><img src="<?php echo base_url(); ?>assets/images/wa.png" alt="" width="50px" style="margin: 10px" /></a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+        <path
+          fill="#f1f1f1"
+          fill-opacity="1"
+          d="M0,96L40,90.7C80,85,160,75,240,69.3C320,64,400,64,480,90.7C560,117,640,171,720,181.3C800,192,880,160,960,160C1040,160,1120,192,1200,202.7C1280,213,1360,203,1400,197.3L1440,192L1440,320L1400,320C1360,320,1280,320,1200,320C1120,320,1040,320,960,320C880,320,800,320,720,320C640,320,560,320,480,320C400,320,320,320,240,320C160,320,80,320,40,320L0,320Z"
+        ></path>
+      </svg>
+    </section>
+    <!-- Social Media -->
---
+++ application/views/templates/header.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous" />
+    <link rel="stylesheet" type="text/css" href="<?= base_url(); ?>assets/css/style.css" />
+    <script src="https://kit.fontawesome.com/6e1d7b8491.js"></script>
+    <title>ianyola</title>
+  </head>
+  <body>
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg navbar-dark fixed-top" style="background-color: #626f7a">
+      <div class="container">
+        <a class="navbar-brand" href="#">Ianyola Official Website</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav ms-auto">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#about">About</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#channel">YouTube</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#social">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <!-- Akhir navbar -->
